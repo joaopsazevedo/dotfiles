7e4b8e312837227d40f8be74a14a6ef2357512ff
Joao Azevedo <joaopsazevedo@outlook.com>
2023-07-13 19:44:52 +0100

    Tidy up als configuration

--- neovim/.config/nvim/lua/lsp/init.lua
+++ neovim/.config/nvim/lua/lsp/init.lua
@@ -2,14 +2,23 @@ require("mason").setup()
 
 -- Setup language servers.
 local lspconfig = require('lspconfig')
-lspconfig.pyright.setup {}
-lspconfig.tsserver.setup {}
-lspconfig.rust_analyzer.setup {
-  -- Server-specific settings. See `:help lspconfig-setup`
-  settings = {
-    ['rust-analyzer'] = {},
-  },
+local get_als_settings = function()
+    local als_settings = io.open('.als-settings.json', 'r');
+    if als_settings then
+        local content = als_settings:read("*a")
+        io.close(als_settings)
+        return {
+            ada = vim.json.decode(content)
+        }
+    end
+    return {}
+end;
+lspconfig.als.setup {
+    settings = get_als_settings()
 }
+lspconfig.pyright.setup {}
+lspconfig.rust_analyzer.setup {}
+lspconfig.tsserver.setup {}
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
@@ -34,11 +43,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, opts)
     vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
