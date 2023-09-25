01b4405223202afbe6f5a51a1cd2eaf2b5f344dd
Joao Azevedo <joaopsazevedo@outlook.com>
2023-09-25 07:06:23 +0100

    Improve LSP key bindings

--- neovim/.config/nvim/lua/lsp/init.lua
+++ neovim/.config/nvim/lua/lsp/init.lua
@@ -19,6 +19,21 @@ lspconfig.als.setup {
 lspconfig.pyright.setup {}
 lspconfig.rust_analyzer.setup {}
 lspconfig.tsserver.setup {}
+lspconfig.gopls.setup({
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = true,
+      gofumpt = true,
+    },
+  },
+})
+
+vim.diagnostic.config({
+    virtual_text = true
+})
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
@@ -40,13 +55,14 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local opts = { buffer = ev.buf }
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<space>gt', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<space>ho', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', '<space>sh', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>fr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>re', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<space>co', vim.lsp.buf.completion, opts)
     vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
     vim.keymap.set('n', '<space>f', function()
       vim.lsp.buf.format { async = true }
     end, opts)
